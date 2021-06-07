a0c4024cfa436631c63e2b7ae6154d1d03faf1c0
LamNhac <63506861+LamNhac@users.noreply.github.com>
2021-06-07 16:50:04 +0700

    commit

--- core/settings/base.py
+++ core/settings/base.py
@@ -7,7 +7,7 @@ SECRET_KEY = "3xk*)i0x#k$btl=(6q)te!19=mp6d)lm1+zl#ts4ewxi3-!vm_"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "codesim.herokuapp.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "codesim.herokuapp.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
